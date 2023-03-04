d23e9a6d97504e6967faed84feab763d258f57ab
vladkeig <introwwert@gmail.com>
2023-03-05 02:53:48 +0300

    v0.2.1 fix

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,23 @@
 use std::process::exit;
-use std::sync::{Arc, Mutex};
+use std::sync::{Mutex};
 
 use argparse::{ArgumentParser, Store};
 
 use hyper::{Client, Uri};
-
-use lazy_static::lazy_static;
+use hyper::client::HttpConnector;
 
 use tokio::signal::unix::{signal, SignalKind};
 use tokio::time::{Duration, Instant};
 use tokio::time;
 
-static REQ_TIME: Mutex<Vec<u32>> = Mutex::new(Vec::new());
+
+mod utils;
+use crate::utils::response::ResponseTime;
+
+
 static ERRORS: Mutex<u128> = Mutex::new(0);
+static RESPONSE: Mutex<ResponseTime> = Mutex::new(ResponseTime::new());
+
 
 #[tokio::main]
 async fn main() {
@@ -49,86 +54,63 @@ async fn main() {
 
     let mut interval = time::interval(Duration::from_micros(1_000_000 / rps as u64));
 
+    let client = Client::new();
+
     // main worker thread
     tokio::spawn(async move {
         loop {
             let url = url.clone();
+            let client = client.clone();
 
             tokio::spawn(async move {
-                get(url).await;
+                get(url, client).await;
             });
 
             interval.tick().await;
         }
     });
 
-
     // shutdown signal check
     let mut stream = signal(SignalKind::interrupt()).unwrap();
-
-    let end = start.elapsed();
     stream.recv().await;
+    let end = start.elapsed();
 
 
     // block of result print
     {
-        let req = REQ_TIME.lock().unwrap().to_vec();
+        let req = RESPONSE.lock().unwrap();
         let err = *ERRORS.lock().unwrap();
-
-        let min: u32 = match req.iter().min() {
-            Some(min) => *min,
-            None => 0
-        };
-        let max: u32 = match req.iter().max() {
-            Some(max) => *max,
-            None => 0
-        };
-
-        let sum = req.iter().sum::<u32>() as u128;
-        let average: u32 = {
-            if sum != 0 {
-                (sum as u32 / req.len() as u32) as u32
-            } else {
-                0
-            }
-        };
 
         print!("\n\n");
         println!("Elapsed:             {:.2?}", end);
-        println!("Requests:            {}", &req.len());
+        println!("Requests:            {}", req.get_count());
         println!("Errors:              {}", err);
-        println!("Percent of errors:   {:.2}%", percent_of_errors(req.len(), &err));
+        println!("Percent of errors:   {:.2}%", percent_of_errors(req.get_count(), &err));
         println!("Response time: \
                 \n - Min:              {}ms \
                 \n - Max:              {}ms \
-                \n - Average:          {}ms", min, max, average);
+                \n - Average:          {}ms", req.get_min(), req.get_max(), req.get_average());
     }
 
     exit(0)
 }
 
-async fn get(uri: Uri) {
+async fn get(uri: Uri, client: Client<HttpConnector>) {
 
     let start = Instant::now();
 
-    let client = Client::new();
-
-    let resp = client.get(uri).await;
-
-    {
-        REQ_TIME
-            .lock()
-            .unwrap()
-            .push(start.elapsed().as_millis() as u32);
+    match client.get(uri).await {
+        Ok(res) => {
+            if !res.status().is_success() {
+                *ERRORS.lock().unwrap() += 1;
+            }
+        },
+        Err(_) => {
+            *ERRORS.lock().unwrap() += 1;
+        }
     }
 
-    if resp.is_err() {
-        *ERRORS.lock().unwrap() += 1;
-        return;
-    }
-    if !resp.unwrap().status().is_success() {
-        *ERRORS.lock().unwrap() += 1;
-    }
+    RESPONSE.lock().unwrap().add(start.elapsed().as_millis() as u32);
 }
 
 fn parse_url(url: String) -> Uri {
@@ -143,13 +125,11 @@ fn parse_url(url: String) -> Uri {
     println!("App work only with HTTP!");
     exit(1)
 }
-fn percent_of_errors(req: usize, err: &u128) -> f32 {
 
-    let req = req as u128;
-
+fn percent_of_errors(req: u32, err: &u128) -> f32 {
     let res = (*err as f32 / req as f32) * 100.0;
     if res > 0 as f32 {
-        return res
+        res
     } else {
         0 as f32
     }
---
+++ src/utils.rs
@@ -0,0 +1,47 @@
+pub mod response {
+    pub struct ResponseTime {
+        average: u32,
+        count: u32,
+        min: u32,
+        max: u32
+    }
+
+    impl ResponseTime {
+        pub const fn new() -> Self {
+            Self {
+                average: 0,
+                count: 0,
+                min: 999_999_999,
+                max: 0
+            }
+        }
+
+        pub fn get_average(&self) -> u32 {
+            self.average
+        }
+        pub fn get_count(&self) -> u32 {
+            self.count
+        }
+        pub fn get_min(&self) -> u32 {
+            self.min
+        }
+        pub fn get_max(&self) -> u32 {
+            self.max
+        }
+
+        pub fn add(&mut self, new: u32) {
+            self.average = (self.average * self.count + new) / (self.count + 1);
+            self.count += 1;
+            self.min_check(new);
+            self.max_check(new);
+        }
+
+        fn min_check(&mut self, item: u32) {
+            self.min = self.min.min(item);
+        }
+
+        fn max_check(&mut self, item: u32) {
+            self.max = self.max.max(item);
+        }
+    }
+}
